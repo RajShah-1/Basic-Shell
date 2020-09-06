953af7f022e1ff4a522475cd360e8010669b4730
RajShah-1 <srk13@iitbbs.ac.in>
2020-09-06 15:42:37 +0530

    Adding history and background jobs feature (In progress)

--- BasicSh.c
+++ BasicSh.c
@@ -4,30 +4,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define PATH_MAX 100            // Max length for the cwd
 #define MACHINE_LENGTH_MAX 100  // Max length for the machine length
 #define PROMPT_LENGTH 204       // Max length of the prompt
+#define CMD_MAX 100             // Max length for the cmd_buff
 #define ARG_BUFSIZE 64          // Max number of supported arg
-#define ARG_DELIM " \t\r\n\a"
+#define ARG_DELIM " \t\r\n\a"   // Delimeters to seperate args
+#define MAX_BG_PROCS 100        // Max num of background jobs at any given time
+#define MAX_HIST 10  // Max num of history entries stored in the queue
+
+typedef struct procInfo {
+  char cmd_buff[CMD_MAX];
+  unsigned int id;
+  pid_t pid;
+  struct procInfo* next;
+} procInfo;
+
+typedef struct {
+  procInfo* front;
+  procInfo* back;
+  int size;
+  int maxSize;
+  int processID;
+} Queue;
+
+// Global Variables
+Queue history;
+Queue jobs;
 
 void cd(char* argPath, char* cwd);
 char** parseCMD(char* cmd_buff);
 char* buildPrompt(char* prompt, char* hostname, char* path);
 int isBuiltIn(char** args);
 void runBuiltIn(char** args, char* path);
+int isBackgroundJob(char** args);
+void sigchldHandler(int sigNum);
+
+// Queue operations
+void enqueue(Queue* Q, const char* cmd_buff, pid_t pid);
+void removePID(Queue* Q, pid_t pid);
+char* getPrevCMD(Queue* Q, int id);
+void printJobs(Queue* Q);
+void printHistory(Queue* Q);
 
 int main() {
   char* cmd_buff;
+  char cmd_buff_org[CMD_MAX];
   char path[PATH_MAX];
   char hostname[MACHINE_LENGTH_MAX];
   char prompt[PROMPT_LENGTH];
-  int childPid;
-  int isBackground = 0;
+  pid_t childPid;
+  int historyID;
+
+  history.front = history.back = NULL;
+  history.processID = history.size = 0;
+  history.maxSize = MAX_HIST;
+
+  jobs.front = jobs.back = NULL;
+  jobs.processID = jobs.size = 0;
+  jobs.maxSize = MAX_BG_PROCS;
 
   // Get the host name
   gethostname(hostname, MACHINE_LENGTH_MAX);
+
+  // Set up SIGCHLD handler
+  signal(SIGCHLD, sigchldHandler);
 
   while (1) {
     // Build path
@@ -37,12 +81,12 @@ int main() {
     }
 
     cmd_buff = readline(buildPrompt(prompt, hostname, path));
+    strcpy(cmd_buff_org, cmd_buff);
 
     // Support up and down arrows to show previously used commands
     if (strlen(cmd_buff) > 0) {
       // Add to history only if the command is not empty
       add_history(cmd_buff);
-      // Custom function to keep track of the history
     }
 
     // Parse cmd_buff
@@ -52,6 +96,30 @@ int main() {
       printf("Enter a valid command!\n");
       continue;
     }
+
+    if (args[0][0] == '!') {
+      char* tmp = args[0] + 1;
+      historyID = atoi(tmp);
+      // printf("History-ID: %d\n", historyID);
+      cmd_buff = getPrevCMD(&history, historyID);
+      free(args);
+      if (cmd_buff == NULL) {
+        printf("Command does not exist in the history!\n");
+        continue;
+      }
+      strcpy(cmd_buff_org, cmd_buff);
+      printf("Executing: %s\n", cmd_buff);
+      // If we pass cmd_buff to parseCMD we'll end up mutating the cmd_buff entry 
+      // in the history queue
+      // So pass cmd_buff_org to parseCMD and then restore it from cmd_buff
+      args = parseCMD(cmd_buff_org);
+      strcpy(cmd_buff_org, cmd_buff);
+      if (args[0] == NULL) {
+        continue;
+      }
+    }
+
+    enqueue(&history, cmd_buff_org, -1);
 
     if (isBuiltIn(args)) {
       if (!strcmp(args[0], "cd")) {
@@ -64,19 +132,13 @@ int main() {
         // free(cmd_buff);
         break;
       } else if (!strcmp(args[0], "jobs")) {
+        printJobs(&jobs);
       } else if (!strcmp(args[0], "history")) {
+        printHistory(&history);
       } else if (!strcmp(args[0], "kill")) {
-      } else if (!strcmp(args[0], "!")) {
       }
       continue;
     }
-    // Check whether the job is a background job or not
-    int index = 0;
-    char* prevArg = NULL;
-    while(args[index+1] != NULL){
-      index++;
-    }
-    isBackground = strcmp(args[index], "&") ? 0:1;
 
     // Create a fork and execute the user command
     childPid = fork();
@@ -86,9 +148,16 @@ int main() {
       exit(EXIT_FAILURE);
     } else if (childPid == 0) {
       // child
-    } else{
+      execvp(args[0], args);
+    } else {
       // Check whether the child process is a background job
-
+      if (isBackgroundJob(args)) {
+        // Record as a background process
+        printf("Background Process started\n");
+        enqueue(&jobs, cmd_buff, childPid);
+      } else {
+        waitpid(childPid, NULL, 0);
+      }
     }
 
     // free(cmd_buff);
@@ -97,6 +166,108 @@ int main() {
   return 0;
 }
 
+procInfo* newProcNode(const char* cmd_buff, pid_t pid, int id) {
+  procInfo* newProc = (procInfo*)malloc(sizeof(procInfo));
+  strcpy(newProc->cmd_buff, cmd_buff);
+  newProc->id = id;
+  newProc->pid = pid;
+  newProc->next = NULL;
+
+  return newProc;
+}
+
+// Queue operations
+void enqueue(Queue* Q, const char* cmd_buff, pid_t pid) {
+  procInfo* newProc = newProcNode(cmd_buff, pid, Q->processID++);
+
+  if (Q->front == NULL) {
+    Q->back = Q->front = newProc;
+    return;
+  }
+
+  if (Q->size >= Q->maxSize) {
+    procInfo* tmp = Q->front;
+    Q->front = Q->front->next;
+    free(tmp);
+  }
+
+  Q->back->next = newProc;
+  Q->back = Q->back->next;
+}
+
+void removePID(Queue* Q, pid_t pid) {
+  // Find and delete the node with pid == pid
+  if (Q->front == NULL) {
+    printf("Warning: Empty queue!");
+    return;
+  }
+
+  procInfo *curr = Q->front, *prev;
+  if (curr->pid == pid) {
+    procInfo* tmp = curr;
+    Q->front = Q->front->next;
+    if (Q->front == NULL) {
+      Q->back = NULL;
+    }
+    free(tmp);
+    return;
+  }
+
+  while (curr && curr->pid != pid) {
+    prev = curr;
+    curr = curr->next;
+  }
+  if (curr == NULL) {
+    printf("Warning: pid not found!");
+    return;
+  }
+  prev->next = curr->next;
+  free(curr);
+  if (prev->next == NULL) {
+    Q->back = prev;
+  }
+}
+
+char* getPrevCMD(Queue* Q, int id) {
+  if (Q->front == NULL) return NULL;
+  if (id == -1) return Q->back->cmd_buff;
+
+  procInfo* tmp = Q->front;
+  while (tmp) {
+    if (tmp->id == id) return tmp->cmd_buff;
+    tmp = tmp->next;
+  }
+  return NULL;
+}
+
+void printJobs(Queue* Q) {
+  procInfo* tmp = Q->front;
+  printf("Background jobs: \n");
+  while (tmp) {
+    printf("NUM(pid): %d - %s\n", tmp->pid, tmp->cmd_buff);
+    tmp = tmp->next;
+  }
+}
+
+void printHistory(Queue* Q) {
+  procInfo* tmp = Q->front;
+  printf("Previous Commands: \n");
+  while (tmp) {
+    printf("ID: %d - %s\n", tmp->id, tmp->cmd_buff);
+    tmp = tmp->next;
+  }
+}
+
+int isBackgroundJob(char** args) {
+  int index = 0;
+  char* prevArg = NULL;
+  while (args[index + 1] != NULL) {
+    index++;
+  }
+  return strcmp(args[index], "&") ? 0 : 1;
+}
+
+// Checks whether args[0] is a builtin command or not
 int isBuiltIn(char** args) {
   if (args[0] == NULL) return 0;
   if (!strcmp(args[0], "cd") || !strcmp(args[0], "ls") ||
@@ -108,7 +279,7 @@ int isBuiltIn(char** args) {
   return 0;
 }
 
-// Returns pointer to prompt
+// Returns pointer to prompt just for convenience
 char* buildPrompt(char* prompt, char* hostname, char* path) {
   // Build the prompt (with colors!)
   strcpy(prompt, "\033[1;32m");  // Bold green
@@ -122,7 +293,7 @@ char* buildPrompt(char* prompt, char* hostname, char* path) {
 }
 
 // Parses the entered command into args
-// (Does not support white spaces in argument as of now)
+// (Does not support white spaces in arguments as of now)
 char** parseCMD(char* cmd_buff) {
   char** args = (char**)malloc(ARG_BUFSIZE * sizeof(char));
   int position = 0;
@@ -146,7 +317,7 @@ char** parseCMD(char* cmd_buff) {
 // Supports both relative and absolute paths
 void cd(char* argPath, char* cwd) {
   if (argPath == NULL) {
-    printf("Enter a valid path!");
+    printf("Enter a valid path!\n");
     return;
   }
 
@@ -164,6 +335,17 @@ void cd(char* argPath, char* cwd) {
     status = chdir(argPath);
   }
   if (status == -1) {
-    printf("Enter a valid path!");
+    printf("Enter a valid path!\n");
+  }
+}
+
+// Waits for all the terminated childs to avoid "Zombies"
+void sigchldHandler(int signum) {
+  while (1) {
+    pid_t pid = waitpid(-1, NULL, WNOHANG);
+    if (pid <= 0) {
+      break;
+    }
+    removePID(&jobs, pid);
   }
 }
--- Hello.c
+++ Hello.c
@@ -1,8 +1,10 @@
+#include <unistd.h>
 #include <stdio.h>
 
 int main() {
   printf("Hello World\n");
   int A;
+  printf("Enter an int: ");
   scanf("%d", &A);
   printf("You entered: %d\n", A);
   return 0;
